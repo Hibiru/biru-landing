a7d8939aecae4ffed8153d0db1d7f332bf4c2670
Vincent Lefebvre <vinc.lefebv@gmail.com>
2018-11-01 16:37:43 +0100

    feat(deviceoriented): WIP on the biry logo movement on device orientation

---
+++ frontends/landing-create-app/src/components/DeviceOriented/container.js
@@ -0,0 +1,132 @@
+import React, { Component, Fragment } from 'react';
+
+class DeviceOriented extends Component {
+  constructor(props) {
+    super(props);
+    this.deviceRef = React.createRef();
+    window.addEventListener('deviceorientation', (event) => {
+      // remember to use vendor-prefixed transform property
+      // this.deviceRef.current.style.transform =
+      //   `rotateX(${event.beta}deg)` +
+      //   ` rotateY(${-event.gamma}deg)` +
+      //   ` rotateZ(${event.alpha - 180}deg)`;
+
+      // RotateX
+      if (!this.initialBeta) this.initialBeta = event.beta;
+      let newBeta = this.initialBeta - event.beta;
+      newBeta = newBeta > 25 ? 25 : newBeta;
+      newBeta = newBeta < -25 ? -25 : newBeta;
+
+      // RotateY
+      if (!this.initialGamma) this.initialGamma = event.gamma;
+      let newGamma = this.initialGamma - event.gamma;
+      newGamma = newGamma > 35 ? 35 : newGamma;
+      newGamma = newGamma < -35 ? -35 : newGamma;
+
+      // RotateZ
+      if (!this.initialAlpha) this.initialAlpha = event.alpha;
+      let newAlpha = this.initialAlpha - event.alpha;
+      let borne1;
+      let borne2;
+      let newAlpha1;
+      let newAlpha2;
+      if (Math.abs(newAlpha) < 180) {
+        newAlpha1 = newAlpha < -35 ? -35 : newAlpha;
+        borne1 = this.initialAlpha - 325;
+      } else {
+        borne2 = this.initialAlpha - 325;
+        newAlpha2 = Math.abs(newAlpha) < Math.abs(borne2) ? borne2 : newAlpha;
+      }
+      // if (newAlpha < 180) newAlpha = -newAlpha;
+      // newAlpha = newAlpha < -35 ? -35 : newAlpha;
+      // newAlpha = newAlpha < -35 ? -35 : newAlpha;
+      // newAlpha = newAlpha > this.initialAlpha + 35 ? 35 : newAlpha;
+      // newAlpha = newAlpha < this.initialAlpha - 35 ? -35 : newAlpha;
+      // const getSignedNumber = (number, signedNumber) => (
+      //   Math.sign(signedNumber) ? number : number * -1
+      // );
+      // newAlpha = Math.abs(newAlpha) > 22 ? getSignedNumber(22, newAlpha) : newAlpha;
+
+      this.setState({
+        initialBeta: Math.round(this.initialBeta),
+        eventBeta: Math.round(event.beta),
+        newBeta: Math.round(newBeta),
+        initialGamma: Math.round(this.initialGamma),
+        eventGamma: Math.round(event.gamma),
+        newGamma: Math.round(newGamma),
+        initialAlpha: Math.round(this.initialAlpha),
+        eventAlpha: Math.round(event.alpha),
+        newAlpha: Math.round(newAlpha),
+        newAlpha1: Math.round(newAlpha1),
+        newAlpha2: Math.round(newAlpha2),
+        borne1: Math.round(borne1),
+        borne2: Math.round(borne2),
+      });
+      this.deviceRef.current.style.transform =
+      ` rotateZ(${newAlpha}deg)`;
+      // `rotateX(${(newBeta)}deg)` +
+      // ` rotateY(${newGamma}deg)`;
+      // if (this.formerApliedAlpha !== newAlpha) {
+      // }
+      // this.formerApliedAlpha = newAlpha;
+        // ` rotateY(${this.gamma - event.gamma / 3}deg)`;
+        // ` rotateZ(${this.alpha - event.alpha / 3}deg)`;
+    });
+    this.state = {
+      initialBeta: null,
+      eventBeta: null,
+      newBeta: null,
+      initialGamma: null,
+      eventGamma: null,
+      newGamma: null,
+      initialAlpha: null,
+      eventAlpha: null,
+      newAlpha: null,
+      newAlpha1: null,
+      newAlpha2: null,
+      borne1: null,
+      borne2: null,
+    };
+  }
+
+  render() {
+    const { children } = this.props;
+    // const { initialBeta, eventBeta, newBeta } = this.state;
+    // const { initialGamma, eventGamma, newGamma } = this.state;
+    const { initialAlpha, eventAlpha, newAlpha, newAlpha1, newAlpha2, borne1, borne2 } = this.state;
+    console.log('initialAlpha', initialAlpha)
+    console.log('eventAlpha', eventAlpha)
+    console.log('newAlpha', newAlpha)
+    console.log('')
+    return (
+      <Fragment>
+        {/* initialBeta: {initialBeta}
+        <br/>
+        eventBeta: {eventBeta}
+        <br/>
+        newBeta: {newBeta}
+        <br/> */}
+        initialAlpha: {initialAlpha}
+        <br/>
+        eventAlpha: {eventAlpha}
+        <br/>
+        newAlpha: {newAlpha}
+        <br/>
+        newAlpha1: {newAlpha1} | {borne1 ? `borne1: ${borne1}` : ''}
+        <br/>
+        newAlpha2: {newAlpha2} | {borne2 ? `borne2: ${borne2}` : ''}
+        <br/>
+        <br/>
+        {/* initialGamma: {initialGamma}
+        <br/>
+        eventGamma: {eventGamma}
+        <br/>
+        newGamma: {newGamma}
+        <br/> */}
+        {React.cloneElement(children, { ref: this.deviceRef })}
+      </Fragment>
+    );
+  }
+}
+
+export default DeviceOriented;
--- frontends/landing-create-app/src/components/Landing/component.js
+++ frontends/landing-create-app/src/components/Landing/component.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 
 import logo from './logo.png';
 import Emoji from '../Emoji';
+import DeviceOriented from '../DeviceOriented';
+import Tilt from 'react-tilt'
 
 import styles from './Landing.module.scss';
 
@@ -11,26 +13,28 @@ class Landing extends Component {
       <div className={styles.landing}>
         <div className={styles.ctaPanel}>
           <div className={styles.logoWrapper}>
-            <img src={logo} className={styles.logo} alt="logo-biry" />
+            <DeviceOriented>
+              <img src={logo} className={styles.logo} alt="logo-biry" />
+            </DeviceOriented>
           </div>
           <h6 className={styles.catchPhrase}>
             "Hello ! Je suis un chatbot qui vas t'aider à trouver <strong>les meilleurs bars</strong> autour de toi ! <Emoji label="Bière">🍻</Emoji>"
           </h6>
-          <buton className={styles.ctaButton}>
-            Viens discuter! <Emoji label="Feu">🔥</Emoji>
-          </buton>
+          <button className={styles.ctaButton}>
+            Viens discuter! <Emoji label="Feu" children="🔥" />
+          </button>
         </div>
         <div className={styles.messagingPanel}>
           <div className={styles.messageRow}>
             <div className={styles.messageBubble_left}>
-              Hello toi ! <Emoji label="Coucou">👋</Emoji>
+              Hello toi ! <Emoji label="Coucou" children="👋" />
               <br />
               moi c’est Biru, le chatbot qui t’aide à trouver les bars sympas autour de toi
             </div>
           </div>
           <div className={styles.messageRow}>
             <div className={styles.messageBubble_right}>
-              Cool <Emoji label="Cool">🤘</Emoji>
+              Cool <Emoji label="Cool" children="🤘" />
               <br />
               Mais…commet ça marche ?
             </div>
@@ -50,6 +54,17 @@ class Landing extends Component {
               Regarde ce que je t’ai trouvé :
             </div>
           </div>
+
+
+          <Tilt className="Tilt" options={{ max : 25 }} style={{ height: 250, width: 250 }} >
+            <img src={logo} className={styles.logo} alt="logo-biry" />
+          </Tilt>
+
+          <div className="your-element" data-tilt data-tilt-max="50" data-tilt-speed="400" data-tilt-perspective="500">
+            Hello
+          </div>
+
+
         </div>
       </div>
     );
